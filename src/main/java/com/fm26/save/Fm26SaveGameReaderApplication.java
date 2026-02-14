98d93fc7c3fc99d0915fcbca5d2a7841d9cf677c
jelmer <jelmer.bouma85@gmail.com>
2026-02-14 13:27:11 +0100

    loading save games with player attributes

--- src/main/java/com/fm26/save/Fm26SaveGameReaderApplication.java
+++ src/main/java/com/fm26/save/Fm26SaveGameReaderApplication.java
@@ -1,9 +1,12 @@
 package com.fm26.save;
 
+import com.fm26.save.savegame.FmSaveProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FmSaveProperties.class)
 public class Fm26SaveGameReaderApplication {
     public static void main(String[] args) {
         SpringApplication.run(Fm26SaveGameReaderApplication.class, args);
